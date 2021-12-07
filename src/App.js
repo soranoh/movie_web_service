248f9148c46c53ba52301b8a993a46f0069afaa3
soranoh <sora2019@naver.com>
2021-12-07 15:08:01 +0900

    #6.0 Introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState } from "react";
 
+/*
+ * state 가 변경이 되변은 App 컴퍼넌트의 모든 코드가 재실행되는 안 좋은 점이 있다.
+ */
 function App() {
+  const [counter, setCounter] = useState(0);
+  const onClick = () => {
+    setCounter((prev) => prev+1);
+  };
+
+  console.log("call API");
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome!</h1>
-      <Button text={"Continue"}/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click</button>
     </div>
   );
 }
