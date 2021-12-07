46062d8da4cd723331c577d40864d0cbd881b2ff
soranoh <sora2019@naver.com>
2021-12-07 15:52:34 +0900

    #6.4 Cleanup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,28 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi :^)");
+    return () => {
+      console.log("bye :^(");
+    };
+  }, []);
 
-/*
- * useEffect 로 지정할 경우 component 의 첫번째 render 시점에만 실행되고,
- * 이후에는 state 가 변경이 되어도 지정된 내용은 재실행되지 않게 보호해준다.
- * 특정 대상의 state 가 변경될 때에만 재실행을 원할 경우
- * useEffect(func, [Array])
- * Array 에 해당되는 대상을 기입한다.
- */
+  return (
+    <h1>Hello</h1>
+  );
+};
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
+  const [showing, setShowing] = useState(false);
   const onClick = () => {
-    setCounter((prev) => prev+1);
+    setShowing((prev) => !prev)
   };
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  }
-
-  useEffect(() => {
-    console.log("run only once")
-  }, []);
-  useEffect(() => {
-    console.log("run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("ruen when 'keyword' or 'counter' changes.");
-  }, [keyword, counter]);
 
   return (
     <div>
-      <input type="text" placeholder="Search here" value={keyword} onChange={onChange} />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
