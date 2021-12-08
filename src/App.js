ab67b513ada392a8f42c3b258a07b4f0bcd76431
soranoh <sora2019@naver.com>
2021-12-08 11:14:23 +0900

    #7.5 React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Detail from "./route/Detail";
+import Home from "./route/Home";
+
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
