5db2c820abc920a6faa6c1cb7147f9947db64424
soranoh <sora2019@naver.com>
2021-12-08 14:04:05 +0900

    #7.6 Parameters

--- src/route/Detail.js
+++ src/route/Detail.js
@@ -1,6 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 function Detail() {
+    const {id} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovie(json.data.movie);
+        setLoading(false);
+    };
+
+    console.log(movie);
+
+    useEffect(() => {
+        getMovie();
+    }, []);
+
     return (
-        <h1>Details</h1>
+        <div>
+            {loading ? (
+                <h1>{"Loading..."}</h1>
+            ) : (
+                <div>
+                    <div style={{backgroundImage:`linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${movie.background_image_original})`}}>
+                        <div style={{padding: "60px 30px", height: "100vh", position: "ralative"}}>
+                            <div style={{width:"300px", float:"left"}}>
+                                <img src={movie.medium_cover_image} alt={movie.title} style={{border:"5px solid white", borderRadius: "4px"}} />
+                            </div>
+                            <div style={{float:"left"}}>
+                                <h1 style={{color: "white"}}>{movie.title}</h1>
+                                <h3 style={{color: "white"}}>{movie.year}</h3>
+                                {movie.genres.map((g) => (<h4 key={movie.id} style={{color: "white"}}>{g}</h4>))}
+                                <h4 style={{color: "white"}}>♥ rating : {movie.rating}</h4>
+                            </div>
+                            <div style={{float:"inline-end", position: "absolute", top: "450px"}}>
+                                <h4 style={{color:"white"}}>{movie.description_full}</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 };
 export default Detail;
