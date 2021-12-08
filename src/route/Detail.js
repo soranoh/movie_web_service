67782dab0a12fd74ee08d307d67b81b871da843d
soranoh <sora2019@naver.com>
2021-12-08 15:24:38 +0900

    #7.9 Styles

--- src/route/Detail.js
+++ src/route/Detail.js
@@ -26,19 +26,19 @@ function Detail() {
                 <h1>{"Loading..."}</h1>
             ) : (
                 <div>
-                    <div style={{backgroundImage:`linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${movie.background_image_original})`}}>
+                    <div style={{backgroundImage:`linear-gradient( rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${movie.background_image_original})`, color: "white"}}>
                         <div style={{padding: "60px 30px", height: "100vh", position: "ralative"}}>
                             <div style={{width:"300px", float:"left"}}>
                                 <img src={movie.medium_cover_image} alt={movie.title} style={{border:"5px solid white", borderRadius: "4px"}} />
                             </div>
                             <div style={{float:"left"}}>
-                                <h1 style={{color: "white"}}>{movie.title}</h1>
-                                <h3 style={{color: "white"}}>{movie.year}</h3>
-                                {movie.genres.map((g) => (<h4 key={movie.id} style={{color: "white"}}>{g}</h4>))}
-                                <h4 style={{color: "white"}}>♥ rating : {movie.rating}</h4>
+                                <h1>{movie.title}</h1>
+                                <h3>{movie.year}</h3>
+                                {movie.genres.map((g) => (<h4 key={g}>{g}</h4>))}
+                                <h4>♥ rating : {movie.rating}</h4>
                             </div>
                             <div style={{float:"inline-end", position: "absolute", top: "450px"}}>
-                                <h4 style={{color:"white"}}>{movie.description_full}</h4>
+                                <h4>{movie.description_full}</h4>
                             </div>
                         </div>
                     </div>
